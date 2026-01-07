add0d5c9680aaeb1480935d3664b7e06b2b5b843
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2026-01-07 08:26:09 +0700

    Lab 1 Completed | Java 5 Spring Boot MVC

---
+++ src/main/java/com/ndyudev/lab1/controller/AuthControllerLesson4.java
@@ -0,0 +1,32 @@
+package com.ndyudev.lab1.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthControllerLesson4 {
+    @Autowired
+    HttpServletRequest request;
+
+    @GetMapping("/lab1/login")
+    public String form() {
+        return "/lab1/login";
+    }
+
+    @PostMapping("/lab1/login")
+    public String login(Model model) {
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+
+        if("ndyudev".equals(user) && "227206".equals(pass)){
+            model.addAttribute("message", "Đăng nhập thành công");
+        } else {
+            model.addAttribute("message", "Tài khoản hoặc mật khẩu sai");
+        }
+        return "/lab1/login";
+    }
+}
