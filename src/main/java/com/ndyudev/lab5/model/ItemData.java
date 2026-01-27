8a2eabaa5ddd59d6b26324ba21276e58b8f7b1f8
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2026-01-27 09:12:44 +0700

    Lab 5 Completed | Java 5 Spring Boot MVC

--- src/main/java/com/ndyudev/lab5/model/ItemData.java
+++ src/main/java/com/ndyudev/lab5/model/ItemData.java
@@ -6,10 +6,10 @@ import java.util.Map;
 public class ItemData {
     public static Map<Integer, Item> items = new HashMap<>();
     static {
-        items.put(1, new Item(1, "Iphone 14 Pro Max", 3099.0, 1));
-        items.put(2, new Item(2, "Samsung Galaxy S23 Ultra", 2690.0, 1));
-        items.put(3, new Item(3, "Xiaomi Mi 13 Pro", 1990.0, 1));
-        items.put(4, new Item(4, "Oppo Find X5 Pro", 17990.0, 1));
-        items.put(5, new Item(5, "Vivo X80 Pro", 16990.0, 1));
+        items.put(1, new Item(1, "Iphone 14 Pro Max", 3099.0, 6));
+        items.put(2, new Item(2, "Samsung Galaxy S23 Ultra", 2690.0, 20));
+        items.put(3, new Item(3, "Xiaomi Mi 13 Pro", 1990.0, 8));
+        items.put(4, new Item(4, "Oppo Find X5 Pro", 17990.0, 72));
+        items.put(5, new Item(5, "Vivo X80 Pro", 16990.0, 37));
     }
 }
