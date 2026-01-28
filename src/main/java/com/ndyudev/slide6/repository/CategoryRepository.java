65a5bf0c0ccc8cc32ec24a4e0eeb8f287ea68fd7
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2026-01-28 23:28:32 +0700

    Slide 6 Progress 25% Spring Data JPA | Java 5 Spring Boot MVC

---
+++ src/main/java/com/ndyudev/slide6/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.ndyudev.slide6.repository;
+
+import com.ndyudev.slide6.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, String> {
+
+}
---
+++ src/main/java/com/ndyudev/slide6/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,48 @@
+package com.ndyudev.slide6.service.impl;
+
+import com.ndyudev.slide6.model.Category;
+import com.ndyudev.slide6.repository.CategoryRepository;
+import com.ndyudev.slide6.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category findById(String id) {
+        Optional<Category> cat = categoryRepository.findById(id);
+        if (cat.isPresent()) return cat.get();
+        return null;
+    }
+
+    @Override
+    public Category create(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public void update(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public Boolean existsById(String id) {
+        return categoryRepository.existsById(id);
+    }
+}
