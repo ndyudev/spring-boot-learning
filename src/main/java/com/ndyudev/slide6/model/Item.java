a61f384606fe88f4d5c2d326730ae4845c48a40c
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2026-01-30 23:09:00 +0700

    stream api and lamda expression

--- src/main/java/com/ndyudev/slide6/model/Item.java
+++
@@ -1,8 +0,0 @@
-package com.ndyudev.slide6.model;
-
-/**
- * Item entity intentionally removed. If you need Item later, recreate this file.
- */
-public class Item {
-    // placeholder
-}
--- src/main/java/com/ndyudev/slide6/service/impl/ItemServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.ndyudev.slide6.service.impl;
-
-/**
- * ItemServiceImpl removed per user request. Placeholder kept to avoid build errors.
- */
-public class ItemServiceImpl {
-    // placeholder
-}
